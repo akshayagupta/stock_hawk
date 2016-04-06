0551799dc7d32b6dc86adbd08e62671368a8d960
Akshaya Gupta <akshaya@zophop.com>
2016-04-07 01:30:24 +0530

    update

---
+++ app/src/main/java/com/sam_chordas/android/stockhawk/widget/StockWidgetIntentService.java
@@ -0,0 +1,18 @@
+package com.sam_chordas.android.stockhawk.widget;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+public class StockWidgetIntentService extends IntentService
+{
+    public StockWidgetIntentService()
+    {
+        super("stockwidgetintnetservice");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent)
+    {
+
+    }
+}
